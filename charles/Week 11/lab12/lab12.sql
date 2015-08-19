6f4004b27ecb11c67320bd29091998c4338ceec5
Charles Frye <charlesfrye@berkeley.edu>
2015-08-19 16:59:43 -0700

    completes lab 12

--- charles/Week 11/lab12/lab12.sql
+++ charles/Week 11/lab12/lab12.sql
@@ -7,18 +7,19 @@
 --   adjacencies(s1, s2): States that are adjacent to each other
 
 create table california as
-  -- REPLACE THIS LINE
-  select 'YOUR CODE HERE';
+  select * from adjacencies where s1 = 'CA';
 
 -- Finds lengths of possible paths between two states
 create table distances as
   with
     distance(start, end, hops) as (
-      -- REPLACE THIS LINE
-      select 'Your', 'Code', 'Here'
+      select s1,s2, from adjacencies union
+      select path.start, hop.s2, path.hops+1
+        from distance as path, adjacencies as hop
+        where path.end = hop.s1 and
+          path.hops < 15
     )
   select * from distance;
 
 create table three_hops as
-  -- REPLACE THIS LINE
-  select 'YOUR CODE HERE';
+  select end from distances where start = "CA" and hops = 3;
--- charles/Week 11/lab12/lab12_extra.sql
+++ charles/Week 11/lab12/lab12_extra.sql
@@ -9,18 +9,42 @@
 create table alphabetical_paths as
   with
     paths(s, n, last) as (
-      -- REPLACE THIS LINE
-      select 'Your', 'Code', 'Here'
+      select s1||','||s2,1,s2 from adjacencies where s2>s1 union
+      select old.s||','||nxt.s2,old.n+1,nxt.s2
+        from paths as old, adjacencies as nxt
+        where nxt.s2 > old.last 
+          and nxt.s1 = old.last
     )
-  select s from paths where n > 6 order by -n;
+  select s,n from paths order by -n;
 
 -- Lengths of possible paths between two states that enter only
 -- landlocked states along the way.
 create table inland_distances as
   with
     inland(start, end, hops) as (
-      -- REPLACE THIS LINE
-      select 'Your', 'Code', 'Here'
+      select a.s1,l.state,1 from adjacencies as a, landlocked as l 
+        where a.s2 = l.state union
+      select paths.start, hop.s2, paths.hops+1
+        from inland as paths, adjacencies as hop
+          where hop.s1 = paths.end
+            and hop.s2 in landlocked
+            and paths.hops < 10
     )
-  -- REPLACE THIS LINE
-  select 'Your' as start, 'Code' as end, 'Here' as hops;
+  select start,end,hops from inland;
+
+create table inland_distances as
+  with
+    inland(start, end, hops) as (
+      select state, state, 0 from landlocked union
+      select start, s2, hops + 1
+        from inland, adjacencies, landlocked
+        where end = s1 and s2 = state and hops < 8
+    )
+  select s1 as start, s2 as end, 2 as hops from adjacencies union
+  select start.s1 as start, end.s2 as end, hops+2 as hops
+    from adjacencies as start, adjacencies as end, inland
+    where start.s2 = start and end.s1 = end;
+
+
+
+    
