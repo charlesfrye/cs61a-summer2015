94e4116623273dddb65cbf660a50da3389018f08
Charles Frye <charlesfrye@berkeley.edu>
2015-08-19 20:48:10 -0700

    completes hw 09

--- charles/Week 11/hw09/hw09.sql
+++ charles/Week 11/hw09/hw09.sql
@@ -29,27 +29,45 @@ create table sizes as
 
 -- The size of each dog
 create table size_of_dogs as
-select "REPLACE THIS LINE WITH YOUR SOLUTION";
+select name,size from dogs, sizes where height > min and height <=max;
 
 
 -- All dogs with parents ordered by decreasing height of their parent
 create table by_height as
-select "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+    select c.name from dogs as c, dogs as p, parents as pedigree
+      where p.name = pedigree.parent and c.name = pedigree.child
+      order by p.height DESC;
 
 -- Sentences about siblings that are the same size
 create table sentences as
-select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  with siblingPairs(kidA, kidB) as (
+    select a.child, b.child from parents as a, parents as b
+      where a.child < b.child and a.parent = b.parent
+    )
+select siblingPairs.kidA || " and " || siblingPairs.kidB ||" are " || a.size|| " siblings"
+  from size_of_dogs as a, size_of_dogs as b, siblingPairs
+    where a.name = siblingPairs.kidA and b.name = siblingPairs.kidB
+    and a.size = b.size;
 
 
 -- Ways to stack 4 dogs to a height of at least 170, ordered by total height
 create table stacks as
-select "REPLACE THIS LINE WITH YOUR SOLUTION";
+    with stacks(list,totHeight,lastHeight,count) as (
+      select name,height,height,1 from dogs union
+      select last.list ||", "|| next.name, last.totHeight + next.height,next.height,last.count+1
+        from stacks as last, dogs as next
+          where last.lastHeight < next.height
+          and last.count <=3
+      )
+select list,totHeight from stacks where count = 4 and totHeight >= 170;
 
 
 create table tallest as
-select "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  with decades(name,height,modulus) as (
+    select name, height, max(height % 10) from dogs 
+      group by height/10 having count(*)>1
+    )
+  select name,height from decades;
 
 -- All non-parent relations ordered by height difference
 create table non_parents as
